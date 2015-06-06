818b7ec536c81460716f5db076db54a9208df6d2
Dan Abramov <dan.abramov@gmail.com>
2015-06-06 18:00:37 +0300

    Way simpler connect API
    Former-commit-id: 66e309782e48bf673c60b63174f53a3a84d02a05
    Former-commit-id: 255bb8c6d56a5158df7af06ac3f0db1423a5faf9

---
+++ src/components/connect.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Connector from './Connector';
+import getDisplayName from '../utils/getDisplayName';
+import shallowEqualScalar from '../utils/shallowEqualScalar';
+
+export default function connect(select) {
+  return DecoratedComponent => class ConnectorDecorator {
+    static displayName = `Connector(${getDisplayName(DecoratedComponent)})`;
+
+    shouldComponentUpdate(nextProps) {
+      return !shallowEqualScalar(this.props, nextProps);
+    }
+
+    constructor() {
+      this.renderChild = this.renderChild.bind(this);
+    }
+
+    render() {
+      return (
+        <Connector select={select}>
+          {this.renderChild}
+        </Connector>
+      );
+    }
+
+    renderChild(state) {
+      const { props } = this;
+      return <DecoratedComponent {...props} {...state} />;
+    }
+  };
+}

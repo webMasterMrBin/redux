72cdb61e4a7e8d8b620046fc4111d1437c07080c
Dan Abramov <dan.abramov@gmail.com>
2016-01-28 02:29:25 +0000

    Add first-class support for store enhancers to createStore() API
    Former-commit-id: 6b5c7560883652764bd426e545075d411906408c
    Former-commit-id: 5bb61edc1773ab701d7343aca8f024dfbc4d2236

--- examples/real-world/store/configureStore.dev.js
+++ examples/real-world/store/configureStore.dev.js
@@ -1,15 +1,25 @@
 import { createStore, applyMiddleware } from 'redux'
-import thunkMiddleware from 'redux-thunk'
+import { syncHistory } from 'react-router-redux'
+import { browserHistory } from 'react-router'
+import DevTools from '../containers/DevTools'
+import thunk from 'redux-thunk'
+import api from '../middleware/api'
 import createLogger from 'redux-logger'
 import rootReducer from '../reducers'
+
+const reduxRouterMiddleware = syncHistory(browserHistory)
 
 export default function configureStore(initialState) {
   const store = createStore(
     rootReducer,
     initialState,
-    applyMiddleware(thunkMiddleware, createLogger()),
+    applyMiddleware(thunk, api, reduxRouterMiddleware, createLogger()),
+    DevTools.instrument()
   )
-
+  
+  // Required for replaying actions from devtools to work
+  reduxRouterMiddleware.listenForReplays(store)
+  
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('../reducers', () => {
